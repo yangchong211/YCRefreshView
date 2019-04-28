1457a73d2a573849e3347683c344dd54e05c194c
yangchong211 <yangchong211@163.com>
2019-04-28 10:07:44 +0800

    使用SpanSizeLookup实现复杂网格布局

---
+++ YCRefreshViewLib/src/main/java/org/yczbj/ycrefreshviewlib/refresh/RefreshLayout.java
@@ -0,0 +1,25 @@
+package org.yczbj.ycrefreshviewlib.refresh;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class RefreshLayout extends ViewGroup {
+
+    public RefreshLayout(Context context) {
+        super(context);
+    }
+
+    public RefreshLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RefreshLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
